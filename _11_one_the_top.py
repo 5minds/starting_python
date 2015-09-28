59b25e48835962aed2e6a86de33478fcfe2f367e
Martin Moellenbeck <martin.moellenbeck@5minds.de>
2015-09-28 20:18:59 +0200

    Add a function to demostrate the callable pattern

--- _11_one_the_top.py
+++ _11_one_the_top.py
@@ -4,8 +4,11 @@ class MyClassAsAfunction(object):
 	def __call__(self, **kwargs):
 		print kwargs
 
+def test_fkt():
+	pass
 
 def main():
+	print callable(test_fkt)
 	print callable(MyClassAsAfunction)
 	a = MyClassAsAfunction()
 	print callable(a)
